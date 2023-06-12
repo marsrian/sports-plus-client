18e5cf1889c871f7b898df54027db0a1274c7894
marsrian <marsrian40@gmail.com>
2023-06-12 18:13:59 +0600

    button color changed Home page

--- src/Pages/Home/Carousel/Carousel.jsx
+++ src/Pages/Home/Carousel/Carousel.jsx
@@ -62,6 +62,7 @@ const Carousel = () => {
               </p>
             </div>
           </div>
+          
           <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
             <a href="#slide2" className="btn btn-circle">
               ❮
